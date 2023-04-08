810c3c791cec182ccbded86bd9c235ddd9b35ec7
HernanMP4 <hmunozg@udd.cl>
2023-04-07 23:17:19 -0400

    prueba

--- Muñoz/prueba.py
+++ Muñoz/prueba.py
@@ -1,6 +1,5 @@
 #%%
-import scipy.stats as stats#aqui estan las distribuicones
-#%%
+import scipy.stats as stats#aqui estan las distribuicones: uniform, norm, binom, geom
 ud=stats.uniform.rvs(size=100000,loc=0,scale=10)
 # %%
 import random
@@ -9,7 +8,8 @@ random.random()
 # %%
 random.randint(0,10)
 random.choice([2,4,6,8,9])
-# %%
+# %%    
 random.seed(19)#set the seed to the same values in the randoms
 print([random.uniform(0,10) for x in range(4)])
 # %%
+ 
