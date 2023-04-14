1f46ef7967f7436d4ac67eaa83c5d81889a98050
HernanMP4 <hmunozg@udd.cl>
2023-04-14 18:58:40 -0400

    actualizacion grouped y grafico

--- Muñoz/guia3.py
+++ Muñoz/guia3.py
@@ -5,63 +5,42 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import scipy.stats as stats
-from scipy.stats import poisson
-from scipy.optimize import curve_fit
-from scipy.stats
 import numpy as np
 import powerlaw as pl
 
 
 #%%
 URL = 'https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto1/Covid-19.csv'
+
 data = pd.read_csv(URL)
 #%%
 vals = list(data.columns)[5:-1]
-ids = list(data.columns)[0:5]
+ids = list(data.columns)[:5]
 df_cases_tidy = pd.melt(data, value_vars=vals, id_vars=ids)
 
-#%%
-#a)limpiamos fechas
-df_cases_tidy['fecha'] = pd.to_datetime(df_cases_tidy['variable'])
 
+#%%
+df_cases_tidy= df_cases_tidy.rename(columns={'variable':'fecha'})
+#a)limpiamos fechas
+df_cases_tidy['fecha'] = pd.to_datetime(df_cases_tidy['fecha'], format='%d-%m-%Y')
+df_cases_sn = df_cases_tidy.dropna()
+
+
+# %%
 # Definir las fechas de inicio y fin
 fi= '2020-03-01'
 ff= '2020-05-10'
-# %%
-df_cases_month = df_cases_tidy[(df_cases_tidy['fecha'] >= fi) & (df_cases_tidy['fecha'] <= ff)]
-
-# %%
-df_case_sn = df_cases_month.dropna()
-#ordenando por valores de fechas el df
-df=df_case_sn.sort_values('fecha')
-#cambiamos a tipo str value (dice que es el error, vamos a probar)
+df_cases_month = df_cases_sn[(df_cases_sn['fecha'] >= fi) & (df_cases_sn['fecha'] <= ff)]
 
 #%%
-df_grouped= df.groupby(df['fecha'])['value'].sum().reset_index()
-df_grouped["new_cases"] = df_grouped["value"].diff()
+df_grouped= df_cases_month.groupby(df_cases_month['fecha'])['value'].sum().reset_index()
+df_grouped["new_cases"] = df_grouped["value"].diff().fillna(df_grouped["value"])
 
 #%%
 sns.lineplot(x='fecha',y='new_cases',data=df_grouped)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 # %%
-#mu = stats.poisson.f(df_grouped['new_cases'].dropna())
+mu = stats.poisson.f(df_grouped['new_cases'].dropna())
 # Define the Poisson distribution function
 def poisson_func(k, lamb):
     return poisson.pmf(k, lamb)
@@ -77,8 +56,9 @@ xmin = fit.xmin
 print("Parámetros de ajuste para la curva Poisson: mu =", mu)
 print("Parámetros de ajuste para la curva powerlaw: alpha =", alpha, "xmin =", xmin)
 
-#%%
-plt.hist(df_grouped['value'], bins=50, density=True)
+
+#%%exactamente lo del video 
+plt.hist(df_grouped['new_cases'], bins=50, density=True)
 
 [mean_fit, std_fit] = stats.norm.fit(df_grouped['value'])
 
