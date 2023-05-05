4ce7d71e9f23e1bb7697945713420a54ab60d22a
HernanMP4 <hmunozg@udd.cl>
2023-05-05 15:29:06 -0400

    se agregan guis bien definidas y ordenadas

--- Muñoz/guia3.py
+++ Muñoz/guia3.py
@@ -6,8 +6,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import scipy.stats as stats
 import numpy as np
-import powerlaw
-
+from scipy.stats import powerlaw
 
 #%%
 URL = 'https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto1/Covid-19.csv'
@@ -18,13 +17,11 @@ vals = list(data.columns)[5:-1]
 ids = list(data.columns)[:5]
 df_cases_tidy = pd.melt(data, value_vars=vals, id_vars=ids)
 
-
 #%%
 df_cases_tidy= df_cases_tidy.rename(columns={'variable':'fecha'})
 #a)limpiamos fechas
 df_cases_tidy['fecha'] = pd.to_datetime(df_cases_tidy['fecha'], format='%d-%m-%Y')
 df_cases_sn = df_cases_tidy.dropna()
-
 
 # %%
 # Definir las fechas de inicio y fin
@@ -37,41 +34,26 @@ df_grouped= df_cases_month.groupby(df_cases_month['fecha'])['value'].sum().reset
 df_grouped["new_cases"] = df_grouped["value"].diff().fillna(df_grouped["value"])
 
 #%%
+print(df_grouped['new_cases'].describe())
+
+#para visualizar 
+plt.hist(df_grouped['value'], bins=20)
+plt.xlabel('Valores de mi_columna')
+plt.ylabel('Frecuencia')
+plt.show()
+
+#%%
 sns.lineplot(x='fecha',y='new_cases',data=df_grouped)
 
-# %%
-#mu = stats.poisson.f(df_grouped['new_cases'].dropna())
-# Define the Poisson distribution function
-#def poisson_func(k, lamb):
-#    return poisson.pmf(k, lamb)
-#/
-# Use curve_fit to fit the data to the Poisson distribution function
-#params, cov = curve_fit(poisson_func, np.arange(len(data)), data)
-# Ajuste de la curva powerlaw
-
-#fit = pl.Fit(df_grouped['new_cases'])
-#alpha = fit.alpha
-#xmin = fit.xmin
-
-#print("Parámetros de ajuste para la curva Poisson: mu =", mu)
-#print("Parámetros de ajuste para la curva powerlaw: alpha =", alpha, "xmin =", xmin)
-
-
-#%%exactamente lo del video 
-plt.hist(df_grouped['new_cases'], bins=50, density=True)
-
-[mean_fit, std_fit] = stats.norm.fit(df_grouped['value'])
-
-print(mean_fit)
-print(std_fit)
-
-x = np.linspace(np.min(df_grouped['value']), np.max(df_grouped['value']))
-
-plt.plot(x, stats.norm.pdf(x, mean_fit, std_fit),)
-
-plt.show()
+#%%
+fit = powerlaw.fit(df_grouped['new_cases'])
+exponent = fit[0]
 # %%
 
+
+
+
+#%%
 # Trazar histograma de los datos
 plt.hist(df_grouped['new_cases'], bins=50, density=True)
 
---
+++ Muñoz/trans.R.py
@@ -0,0 +1,59 @@
+#%%
+import numpy as np
+import pandas as pd 
+import seaborn as sns
+import matplotlib.pyplot as plt 
+from scipy.stats import bernoulli,norm
+#%%
+        
+#Perro se come la tarea random
+#aka missing completly at random
+N = 100 #Estos 3 usan el signo "<-" en vez del "=
+S = norm(0,1).rvs(size=N)
+H = norm(0,0.5*S).rvs(size=N)
+sns.scatterplot(data=S)
+sns.scatterplot(data=H)
+#%%
+
+#El perro se come el 50% de la tarea at random
+D = np.random.binomial(n=1, p=0.85, size=N) #D = rbern(N,8.5)
+Hstar = H.copy()
+Hstar[D==1] = np.nan
+
+plt.plot(S, H, c="gray", alpha=0.8, linewidth=2)
+plt.scatter(S, Hstar, c="red", linewidth=3)  #col=2
+plt.show()  
+ 
+ #%%
+ #Perro se come la tarea random
+#NONELINEAR WITH CEILING EFFECT
+N = 100 
+S = np.random.normal(size=N)  
+H = np.random.normal(loc=0, scale=(1-(np.exp(-0.7*S))), size=N )
+ 
+#El perro se come el 100% de la tarea con S>0
+D = np.random.binomial(n=1, p=np.where(S > 0, 1, 0), size=N)
+
+Hstar = H.copy()
+Hstar[D==1] = np.nan 
+
+plt.plot(S, H, c="gray", alpha=0.8, linewidth=2) 
+plt.scatter(S, Hstar, c="red", linewidth=3)  #col=2
+plt.show()  
+
+#%%
+# Perro se come la tarea random
+# NONELINEAR WITH CEILING EFFECT
+N = 100
+S = np.random.normal(size=N)
+H = np.random.normal(loc=0, scale=np.sqrt(1 - 0.7*S**2), size=N)
+
+# El perro se come el 100% de la tarea con S > 0
+D = np.random.binomial(n=1, p=np.where(S > 0, 1, 0), size=N)
+Hstar = H.copy()
+Hstar[D==1] = np.nan 
+
+plt.plot(S, H, c="gray", alpha=0.8, linewidth=2) 
+plt.scatter(S, Hstar, c="red", linewidth=3)  #col=2
+plt.show()  
+# %%
