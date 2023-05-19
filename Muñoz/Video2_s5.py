b49d3ad66ab0715a28067a1133b889d005ea0fb0
HernanMP4 <hmunozg@udd.cl>
2023-05-19 16:37:04 -0400

    se agregan copias de videos

--- Muñoz/Video2_s5.py
+++ Muñoz/Video2_s5.py
@@ -1,41 +1,36 @@
+#%%
 import matplotlib.pyplot as plt
 import scipy.stats
 import csv
 import numpy as np
+#%%
+ data = []
 
-# data = []
-#
-# with open("diner_quality.csv") as file:
-#
-#     reader = csv.reader(file, delimiter=',')
-#     for row in reader:
-#         data.append(float(row[0]))
-#
-#
-# plt.hist(data, bins=100, density=True)
-#
-#
-# [mean_fit, std_fit] = scipy.stats.norm.fit(data)
-#
-# print(mean_fit)
-# print(std_fit)
-#
-# x = np.linspace(np.min(data), np.max(data))
-#
-# plt.plot(x, scipy.stats.norm.pdf(x, mean_fit, std_fit),)
-#
-# plt.xlabel("Restaurant Quality")
-# plt.ylabel("Probability Density")
-#
-# plt.show()
+ with open("diner_quality.csv") as file:
+    
+     reader = csv.reader(file, delimiter=',')
+     for row in reader:
+         data.append(float(row[0]))
+#%%
+
+ plt.hist(data, bins=100, density=True)
 
 
-###################################################
+ [mean_fit, std_fit] = scipy.stats.norm.fit(data)
 
+ print(mean_fit)
+ print(std_fit)
 
+ x = np.linspace(np.min(data), np.max(data))
 
+ plt.plot(x, scipy.stats.norm.pdf(x, mean_fit, std_fit),)
 
+ plt.xlabel("Restaurant Quality")
+ plt.ylabel("Probability Density")
 
+ plt.show()
+
+#%%
 data = []
 
 with open("pizza_shop_quality.csv") as file:
